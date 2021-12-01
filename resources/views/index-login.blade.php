f313d3a616fc7958322fe3f2a02c5f24e8de07fc
JadeClay <isjadeclo@gmail.com>
2021-11-30 22:15:48 -0400

    Created main css stylesheet on public folder

--- resources/views/index-login.blade.php
+++ resources/views/index-login.blade.php
@@ -1,13 +1,38 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ HTML::style('css/css.css'); }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Lilita+One&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+
+    <link rel="stylesheet" href="{{ URL::asset('css/landing-page.css') }}" />
     <title>Joyerias Brador</title>
 </head>
 <body>
-    hola mundo
+    <div class="container">
+        <nav class= "nav-menu">
+            <li><a href="">Joyerias Brador</a></li>
+            <li class="active"><a href=""><span class="inline-icon material-icons">home</span> Inicio</a></li>
+            <li class="hvr-underline-from-center"><a href=""><span class="inline-icon material-icons">people</span> Usuarios</a></li>
+            <li class="hvr-underline-from-center"><a href=""><span class="inline-icon material-icons">admin_panel_settings</span> Clientes</a></li>
+            <li class="hvr-underline-from-center"><a href=""><span class="inline-icon material-icons">sell</span> Ventas</a></li>
+        </nav>
+
+        <div class="landing-text">
+            <p>Joyerias Brador</p>
+        </div>
+
+        <footer>
+
+        </footer>
+    </div>
 </body>
 </html>
+
