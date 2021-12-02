13862ef80a006d7fdf22025016f735ac3f994026
JadeClay <isjadeclo@gmail.com>
2021-12-01 21:08:52 -0400

    Created user controller

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/users', [App\Http\Controllers\HomeController::class, 'index']);
+
 Auth::routes();
 
 
