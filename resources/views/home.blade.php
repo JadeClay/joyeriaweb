5860f0f71b908074f31e5240f5f33e11d7b8682b
JadeClay <isjadeclo@gmail.com>
2021-12-01 20:56:00 -0400

    Footer created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,13 +61,11 @@
 
         <footer>
             <div class="copyright">
-                <p>&copy 2013 - Organisation</p>
+                <p>&copy 2021 - Oscar Piña y Arturo Rodríguez</p>
             </div>
             <div class="social">
-                <a href="#" class="support">Contact Us</a>
+                <a href="#" class="support">Soporte técnico</a>
                 <a href="#" class="face">f</a>
-                <a href="#" class="tweet">t</a>
-                <a href="#" class="linked">in</a>
             </div>
         </footer>
 
